336e226c0e7e7deb7584ff7f1fa1abadb46d9734
Tiew429 <tiewstudy@gmail.com>
2025-04-12 09:15:33 +0800

    1. importing google ml vision

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.fyp_wyc"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
